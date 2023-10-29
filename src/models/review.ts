b571e5bd88d704c4a72f8c8eb4a860c7f3e53b4c
Ali Yunes <younesal99@gmail.com>
2023-10-29 05:43:22 +0200

    Add model for Review

---
+++ src/models/review.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const ReviewSchema = new mongoose.Schema({
+    itemId: {type:String, required: true},
+    userId: {type:String, required: true},
+    title: {type:String, required: true},
+    comment: {type:String, required: true},
+    rating: {type:Number, required: true},
+    dateCreated: {type: Date, default: Date.now},
+    dateModified: {type: Date, default: Date.now},
+});
+
+ReviewSchema.pre("save", function(next) {
+    this.dateModified = new Date();
+    next();
+});
