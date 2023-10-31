8841e33bd2dd53a6c09b31735bf37afa5fb13c7b
Ali Yunes <younesal99@gmail.com>
2023-10-31 08:47:29 +0200

    Add forceSSL middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -25,3 +25,16 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         return res.sendStatus(400);
     }
 }
+
+export const forceSSL = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try{
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+              ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        return next();
+    } catch(error) {
+        console.log(error);
+    }
+}
