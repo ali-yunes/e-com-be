0cb34e5f5515020761a59b4547025d216d7bbb29
Ali Yunes <younesal99@gmail.com>
2023-10-31 08:47:33 +0200

    Add forceSSL middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import products from "./products";
 import auth from "./auth";
+import {forceSSL} from "../middleware";
 
 const router = express.Router();
 
 export default (): express.Router => {
+    router.use(forceSSL);
     products(router);
     auth(router);
     return router;
